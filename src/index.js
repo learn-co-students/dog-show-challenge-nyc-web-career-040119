9e4b3d58b4d66a8064847eb2ccd074e1cabb574b
William Sinn <w.h.sinn@gmail.com>
2019-05-20 22:19:52 -0400

    cleaned up and noted work

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const dogsTable = document.querySelector("#table-body")
 
 
-fetch(dogsUrl, {method: "GET"})
-.then(function(response){
-  return response.json();
-})
-.then(function(dogsObj){
+  fetch(dogsUrl, {
+      method: "GET"
+    })
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(dogsObj){
 
-  dogsObj.forEach(function(dog){
-    dogsTable.innerHTML += `<tr>
+      dogsObj.forEach(function(dog) {
+        dogsTable.innerHTML += `<tr>
                               <td class="name">${dog.name}</td>
                               <td class="breed">${dog.breed}</td>
                               <td class="sex">${dog.sex}</td>
@@ -21,45 +23,23 @@ fetch(dogsUrl, {method: "GET"})
                             </tr>
                             `
 
-  })
-
-  //   let clickedDog = e.target
-  //   let dogValues = clickedDog.parentElement.parentElement.children
-  //   let dogName = dogValues[0].innerText
-  //   let dogBreed = dogValues[1].innerText
-  //   let dogSex = dogValues[2].innerText
-  //
-  //
-  //   const editDogForm = document.querySelector('#dog-form')
-  //   let name = editDogForm.children.name.input.value
-  //   name = dogName;
-  //
-  //   let breed = editDogForm.children.breed.input.value
-  //   breed = dogBreed;
-  //
-  //   let sex = editDogForm.children.sex.input.value
-  //   sex = dogSex;
-  //
-  //     editDogForm.addEventListener("submit", function(e){
-  //       e.preventDefault()
-  //       debugger
-  //
-  //     })
-
-  // })
-
-})//Dogs Listed
+      })
+    }) //END of dogsURL fetch
 
   dogsTable.addEventListener("click", function(e) {
 
-      let clickedDog = e.target
-      let dogId = parseInt(clickedDog.id)
-      let dogValues = clickedDog.parentElement.parentElement.children
-      let dogName = dogValues[0].innerText
-      let dogBreed = dogValues[1].innerText
-      let dogSex = dogValues[2].innerText
-      const editForm = document.querySelector('.form-container')
-      editForm.innerHTML = `
+    let clickedDog = e.target //helper for dogId/dogValues
+    let dogId = parseInt(clickedDog.id) //dogId-currently editing values, stays the same
+
+    //event parent notation found in devtools
+    let dogValues = clickedDog.parentElement.parentElement.children
+    let dogName = dogValues[0].innerText
+    let dogBreed = dogValues[1].innerText
+    let dogSex = dogValues[2].innerText
+
+    //grab form, put current values into respective input.value slots
+    const editForm = document.querySelector('.form-container')
+    editForm.innerHTML = `
                           <form id='dog-form' class="padding margin border-round border-grey">
                             <input type="name" name="name" placeholder="name" value="${dogName}">
                             <input type="breed" name="breed" placeholder="breed" value="${dogBreed}">
@@ -67,57 +47,27 @@ fetch(dogsUrl, {method: "GET"})
                             <input type="submit"value="Submit">
                           </form>
                           `
-      editForm.addEventListener("submit", function(event){
-        let newName = event.target.name
-        let newBreed = event.target.breed
-        let newSex = event.target.sex
-
-
+    editForm.addEventListener("submit", function(event) {
+      // EVENT TARGET the submit for updated text values
+      let newName = event.target.name
+      let newBreed = event.target.breed
+      let newSex = event.target.sex
       fetch(`http://localhost:3000/dogs/${dogId}`, {
-        method:"PATCH",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json"
-        },
-        body: JSON.stringify(
-          {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: JSON.stringify({
+            //Update to latest Values
             "name": newName.value,
             "breed": newBreed.value,
             "sex": newSex.value
-          }
-        )
-      })
-      .then(response => response.json())
-      .then(function(editDog){
-        // let editedDog = document.querySelector(`#dog-${clickDog.id}`).innerHTML
-        // console.log(editedDog)
-      })
+          })
+        })
+        .then(function(editedDogs) {})//rerenders editedObject
+    })
   })
-})
 
 
-})//END of DOMContentLoaded
-
-//
-//
-// const editBtn = document.querySelector('#edit-dog-btn')
-// const editForm = document.querySelector('.form-container')
-// let editDog = false
-//
-// editBtn.addEventListener('click', () => {
-// // hide & seek with the form
-// editDog = !editDog
-// if (editDog) {
-//   let edit = editForm.style.display
-//
-//   // submit listener here
-//   editForm.addEventListener("submit", function(e){
-//     e.preventDefault(e);
-//     console.log("hi")
-//
-//   })
-// } else {
-//   editForm.style.display
-//
-// }
-// })
+}) //END of DOMContentLoaded
